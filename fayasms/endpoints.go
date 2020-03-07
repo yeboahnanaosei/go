a4b5737817d10c34bcf8ba376b05b37b92faa854
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2020-03-07 04:09:19 +0000

    Move checks into endpoints.go
    Initially the functions that performed checks before hitting an
    endpoint where in their own separate file called checks.go. I feel
    this split is not necessary considering that there are only two
    checks and also these checks are being consumed in the endpoints.go
    file. When reading through the code it can be difficult since you
    would see a function being called in endpoints.go but to see the
    definition of the function you would have to switch to another
    file.
    
    If the checks become many we can move them into their own separate
    file. For now keeping them close to where they are used will help
    reduce cognitive friction

--- fayasms/endpoints.go
+++ fayasms/endpoints.go
@@ -2,6 +2,7 @@ package fayasms
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -15,12 +16,21 @@ var endpoints = map[string]string{
 	"new_id":   "https://devapi.fayasms.com/senders/new",
 }
 
-
 // mandatoryFields are required by FayaSMS to be present in every request
 var mandatoryFields = []string{
 	"AppKey", "AppSecret",
 }
 
+// checkMandatoryFields checks to ensure that the mandatory fields are set
+func (f *FayaSMS) checkMandatoryFields(mandatoryFields []string) error {
+	for _, field := range mandatoryFields {
+		if f.payload.Get(field) == "" {
+			return fmt.Errorf("fayasms: a mandatory field has not been set. please supply all mandatory fields which are: %v", mandatoryFields)
+		}
+	}
+
+	return nil
+}
 
 // contingentFields are only required based on the endpoint being hit.
 // This map shows the endpoints and the fields they require
@@ -36,6 +46,23 @@ var contingentFields = map[string][]map[string]string{
 	},
 }
 
+// checkContingentFields checks that all contingent fields required by endpoint are set
+func (f *FayaSMS) checkContingentFields(endpoint string, contingentFields map[string][]map[string]string) error {
+	fields, ok := contingentFields[endpoint]
+
+	// Some endpoints do not have any contingent fields
+	if !ok {
+		return nil
+	}
+
+	for _, field := range fields {
+		if f.payload.Get(field["name"]) == "" {
+			return fmt.Errorf("fayasms: %v", field["errMsg"])
+		}
+	}
+
+	return nil
+}
 
 // exec executes the actual http request by fetching the endpoint
 // to make the request to
